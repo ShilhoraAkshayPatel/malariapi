f9849206af292c64e51bb9b6443276982bfd2baa
Shilhora Akshay Patel <48085621+ShilhoraAkshayPatel@users.noreply.github.com>
2020-07-06 17:33:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,14 +2,13 @@ from flask_cors import CORS
 import base64
 import numpy as np
 import io
-#from PIL import Image
+from PIL import Image
 import keras
 from keras import backend as K
 from keras.models import Sequential
 from keras.models import load_model
 from keras.preprocessing.image import ImageDataGenerator
 from keras.preprocessing.image import img_to_array
-from keras.preprocessing import image
 from flask import request
 from flask import jsonify
 from flask import Flask
@@ -22,11 +21,11 @@ model = pickle.load(open('finalized_model.sav', 'rb'))
 
 
 def preprocess_image(image, target_size):
-    #if image.mode != "RGB":
-        #image = image.convert("RGB")
-    #image = image.resize(target_size)
-    #image = img_to_array(image)
-    #image = np.expand_dims(image, axis=0)
+    if image.mode != "RGB":
+        image = image.convert("RGB")
+    image = image.resize(target_size)
+    image = img_to_array(image)
+    image = np.expand_dims(image, axis=0)
     return image
 
 
@@ -37,23 +36,13 @@ def index():
 
 @app.route('/api/predict', methods=['POST'])
 def predict():
-    #message = request.get_json(force=True)
-    f = request.files['file']
-    image=f.filename
-    #encoded = message['image']
-    #decoded = base64.b64decode(encoded)
-    #image = Image.open(io.BytesIO(decoded))
-    img = image.load_img(image, target_size=(128, 128))
+    message = request.get_json(force=True)
+    encoded = message['image']
+    decoded = base64.b64decode(encoded)
+    image = Image.open(io.BytesIO(decoded))
+    processed_image = preprocess_image(image, target_size=(128, 128))
 
-    # Preprocessing the image
-    x = image.img_to_array(img)
-    # x = np.true_divide(x, 255)
-    x = np.expand_dims(x, axis=0)
-    
-    #processed_image = preprocess_image(x)
-    #processed_image = preprocess_image(image, target_size=(128, 128))
-    prediction = model.predict(x)
-    #prediction = model.predict(processed_image)
+    prediction = model.predict(processed_image)
     preclass = {0: "Parasitized", 1: "Uninfected"}
     response = preclass[np.argmax(prediction)]
     return jsonify(response)
